103066a44dcf66b914e01d0876a39ce7355816e9
Ned Ruggeri <ruggeri@self-loop.com>
2013-06-20 09:36:13 -0700

    add associations

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,3 +1,8 @@
 class Secret < ActiveRecord::Base
-  attr_accessible :text
+  attr_accessible :title, :body
+
+  belongs_to :author, :class_name => "User"
+  belongs_to :recipient, :class_name => "User"
+
+  validates :title, :body, :author, :recipient, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,17 @@ class User < ActiveRecord::Base
   attr_accessible :username, :password
   attr_reader :password
   
+  has_many(
+    :authored_secrets,
+    :class_name => "Secret",
+    :foreign_key => "author_id"
+  )
+  has_many(
+    :received_secrets,
+    :class_name => "Secret",
+    :foreign_key => "recipient_id"
+  )
+  
   validates :username, :password_digest, :presence => true
   
   def password=(password)
