b96d0863732135c85bd51187e287d12c9d23ed9e
Ned Ruggeri <ruggeri@self-loop.com>
2013-06-20 11:16:00 -0700

    fix sessions_helper to use session vs params

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,8 @@
 module SessionsHelper
   def current_user
-    return nil if params[:session_token].nil?
-    
-    User.find_by_session_token(params[:session_token])
+    return nil if session[:session_token].nil?
+
+    User.find_by_session_token(session[:session_token])
   end
 
   def login_user(username, password)
@@ -13,8 +13,8 @@ module SessionsHelper
 
     u.session_token = SecureRandom::urlsafe_base64(32)
     u.save!
-    params[:session_token] = u.session_token
-    
+    session[:session_token] = u.session_token
+
     u
   end
 end
