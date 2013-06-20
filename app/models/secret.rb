ec131fc6fc63e1035f9a77f539ea4c30e2c81004
Ned Ruggeri <ruggeri@self-loop.com>
2013-06-20 09:38:08 -0700

    remove body attribute of Secret

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,8 +1,8 @@
 class Secret < ActiveRecord::Base
-  attr_accessible :title, :body
+  attr_accessible :title
 
   belongs_to :author, :class_name => "User"
   belongs_to :recipient, :class_name => "User"
 
-  validates :title, :body, :author, :recipient, :presence => true
+  validates :title, :author, :recipient, :presence => true
 end
--- db/migrate/20130620163148_create_secrets.rb
+++ db/migrate/20130620163148_create_secrets.rb
@@ -2,7 +2,6 @@ class CreateSecrets < ActiveRecord::Migration
   def change
     create_table :secrets do |t|
       t.string :title, :null => false
-      t.text :body, :null => false
 
       t.integer :author_id, :null => false
       t.integer :recipient_id, :null => false
